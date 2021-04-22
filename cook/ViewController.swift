8341675427d40e228cb0e7cfa7ae0066e3811fe6
kang15567 <kang15567@gmail.com>
2021-04-22 15:19:24 +0900

    first commit

---
+++ cook/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  cook
+//
+//  Created by Kang on 2021/04/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+    }
+
+}
+
